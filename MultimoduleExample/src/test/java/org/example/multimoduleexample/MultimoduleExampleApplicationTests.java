fbac866eda6e8cea485a835611331354acb0186d
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-12-29 23:33:33 +0200

    init commit

---
+++ MultimoduleExample/src/test/java/org/example/multimoduleexample/MultimoduleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.multimoduleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimoduleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
