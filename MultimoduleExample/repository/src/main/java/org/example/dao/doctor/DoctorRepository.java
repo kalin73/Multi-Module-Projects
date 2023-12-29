246c05ccf83a18239f407c01fd6ee435fb363b0e
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-12-29 23:15:12 +0200

    deleting

--- MultimoduleExample/repository/src/main/java/org/example/dao/doctor/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.dao.doctor;
-
-import org.example.model.doctor.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
--- MultimoduleExample/web/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE doctors
-(
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50),
-    specialist VARCHAR(50)
-);
