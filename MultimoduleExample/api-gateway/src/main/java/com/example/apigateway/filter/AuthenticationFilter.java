d29bb0fdad2ca8eb41dbef165dcbc126c580a480
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-01-19 19:11:12 +0200

    Authentication Filter

---
+++ MultimoduleExample/api-gateway/src/main/java/com/example/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.example.apigateway.filter;
+
+import com.example.apigateway.util.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+    private final RouteValidator routeValidator;
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationFilter(RouteValidator routeValidator, JwtUtil jwtUtil) {
+        super(Config.class);
+        this.routeValidator = routeValidator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("Missing authorization header!");
+                }
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+                    jwtUtil.validateToken(authHeader);
+
+                } catch (Exception ex) {
+                    System.out.println("Invalid access!");
+                    throw new RuntimeException("Invalid token");
+                }
+            }
+
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+
+    }
+}
