4d009abcda86c085dd2f09d97d7a72c501922146
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-11 00:26:56 +0200

    Optimizing imports

--- MultimoduleExample/security/src/main/java/com/example/security/web/AuthController.java
+++ MultimoduleExample/security/src/main/java/com/example/security/web/AuthController.java
@@ -4,7 +4,6 @@ import com.example.security.domain.dto.AuthRequest;
 import com.example.security.domain.entity.UserCredential;
 import com.example.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
